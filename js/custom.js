1301918a9c9217cd8c7bb14237561bb7f4ef5249
desipossa <c1jang@hanmail.net>
2024-10-28 14:38:51 +0900

    20241028

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+
+        if (sct > 0) {
+            $('#header').addClass('on')
+        } else {
+            $('#header').removeClass('on')
+        }
+    })
+})
