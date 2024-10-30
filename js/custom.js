699e9bde1c588e68769740beec6bb8d7a9245d05
desipossa <c1jang@hanmail.net>
2024-10-30 14:29:40 +0900

    20241030

--- js/custom.js
+++ js/custom.js
@@ -34,4 +34,22 @@ $(function () {
 
 
     })
-})
+});
+
+
+$(function () {
+    // Initialize Lenis
+    const lenis = new Lenis();
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+});
+
+$(function () {
+    AOS.init();
+});
